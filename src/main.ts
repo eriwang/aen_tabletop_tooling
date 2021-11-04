a86e251ed8ceefd5f13eab62dd93bec6e868bec1
Austin Trieu <austinctrieu@gmail.com>
2021-11-03 19:52:25 -0700

    Adding functionality to read attributes from sheet

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import { AttributeStats } from "attribute_stats";
+
 function sayHi(person: string) {
-    SpreadsheetApp.getUi().alert(`Hi ${person}`);
+    //SpreadsheetApp.getUi().alert(`Hi ${person}`);
+    let character:AttributeStats = new AttributeStats(person);
 }
 
 function calculateDamage() {
-    sayHi('Elmo');
+    sayHi('Elmo Elless');
 }
