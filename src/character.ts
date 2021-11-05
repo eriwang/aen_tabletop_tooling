dce12e95577a40eb214eac581146ee2c7fc43b46
Eric Wang <eriwang@users.noreply.github.com>
2021-11-04 20:38:06 -0400

    Add base damage, rework multiplier on weapon (#8)

--- src/character.ts
+++ src/character.ts
@@ -4,11 +4,13 @@ import { ResistanceStats } from 'resistance_stats';
 
 // This is here for now since there's not much special about weapons (yet)
 export interface Weapon {
+    // Note that there can be multiple attributes here (and one multiplier per attribute), it's just not implemented yet
     attribute: Attribute;
     attackType: AttackType;
-    toHitMultiplier: number;
-    difficultyClass: number;
     damageType: DamageType;
+    baseDamage: number;
+    attributeMultiplier: number;
+    difficultyClass: number;
 }
 
 export class Character {
@@ -20,5 +22,12 @@ export class Character {
         this.attributeStats = attrStats;
         this.resistanceStats = resStats;
         this.weapon = weap;
+    }
+
+    getScalingFactor() : number {
+        return Math.ceil(
+            this.attributeStats.get(this.weapon.attribute) *
+            this.weapon.attributeMultiplier
+        );
     }
 }
