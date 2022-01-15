a6abdd774ac6f260a67559685abb009015452f54
Eric Wang <johnericcw@gmail.com>
2022-01-15 15:46:19 -0600

    Support non required fields

--- functions/src/ability.ts
+++ functions/src/ability.ts
@@ -3,16 +3,18 @@ import * as yup from 'yup';
 // TODO: not all of these will be mandatory due to actives/ passives/ ultimates being different. Some thought should go
 //       into how the class itself should handle this.
 export const abilitySchema = yup.object().shape({
-    attribute: yup.number().required(),  // TODO: string transformed to enum
-    baseDamage: yup.number().required(),
-    category: yup.string().required(),  // TODO: string transformed to enum, passive basic or ult
+    name: yup.string().required(),
+    category: yup.string().required(),
     cooldown: yup.number().required(),
-    damageType: yup.number().required(),  // TODO: string transformed to enum
     description: yup.string().required(),
     fpCost: yup.number().required(),
-    hitDC: yup.number().required(),
-    range: yup.number().required(),
-    attackType: yup.number().required(),  // TODO: string transformed to enum, I think this is "type" in console atm
+
+    attribute: yup.string(),
+    baseDamage: yup.number(),
+    damageType: yup.string(),
+    hitDC: yup.number(),
+    range: yup.number(),
+    attackType: yup.string(),
 });
 
 export interface AbilityData extends yup.InferType<typeof abilitySchema> {}
--- functions/src/tools/modify_ability_schema.ts
+++ functions/src/tools/modify_ability_schema.ts
@@ -1,12 +1,47 @@
 import { collection, getDocs, setDoc, getFirestore } from 'firebase/firestore';
 
+import { abilitySchema, AbilityData } from 'ability';
 import { run } from 'tools/firestore_tool';
+
+function setIfStringNonEmpty(abilityData: AbilityData, key: string, s: string) {
+    if (s !== '') {
+        (abilityData as any)[key] = s;
+    }
+}
+
+function setIfNumNonZero(abilityData: AbilityData, key: string, n: number) {
+    if (n !== 0) {
+        (abilityData as any)[key] = n;
+    }
+}
 
 run(async () => {
     const db = getFirestore();
 
     const querySnapshot = await getDocs(collection(db, 'Abilities'));
+    const abilityData: AbilityData[] = [];
     querySnapshot.forEach((doc) => {
-        console.log(doc.id);
+        const d = doc.data();
+        const docAbilityData: AbilityData = {
+            name: doc.id,
+            category: d.Category,
+            cooldown: d.Cooldown,
+            description: d.Description,
+            fpCost: d.FPCost,
+        };
+
+        setIfStringNonEmpty(docAbilityData, 'attribute', d.Attribute);
+        setIfStringNonEmpty(docAbilityData, 'damageType', d.DamageType);
+        setIfStringNonEmpty(docAbilityData, 'attackType', d.Type);
+        setIfNumNonZero(docAbilityData, 'baseDamage', d.BaseDamage);
+        setIfNumNonZero(docAbilityData, 'hitDC', d.HitDC);
+        setIfNumNonZero(docAbilityData, 'range', d.Range);
+
+        abilitySchema.validateSync(docAbilityData);
+        abilityData.push(docAbilityData);
+    });
+
+    abilityData.forEach((d) => {
+        console.log(d);
     });
 });
