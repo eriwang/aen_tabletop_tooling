03eab9ff0a9f02fea5073df95dc23cb7ea855bbf
Eric Wang <eriwang@users.noreply.github.com>
2022-01-17 11:18:42 -0600

    Allow Abilities in calculateAttack + frontend changes (#50)

--- functions/src/firestore_tests/src_firebase_functions/calculate_attack.test.ts
+++ functions/src/firestore_tests/src_firebase_functions/calculate_attack.test.ts
@@ -1,23 +1,23 @@
 import calculateAttack from 'src_firebase_functions/calculate_attack';
 
 import * as admin from 'firebase-admin';
-import test from 'firebase-functions-test';
+import fftest from 'firebase-functions-test';
 import { initializeTestEnvironment, RulesTestEnvironment } from '@firebase/rules-unit-testing';
 
 import * as attackCalculator from 'attack_calculator';
 import { getCharacterRepr } from 'tests/test_data';
 import { Character } from 'character';
 import { WeaponData } from 'weapon';
+import { AbilityData } from 'ability';
 
 let testCollection: admin.firestore.CollectionReference;
 let testEnv: RulesTestEnvironment;
 
 let testWeapon: WeaponData;
-let testData: {
-    [key: string]: string | number,
-};
+let testAbility: AbilityData;
+let testData: any;
 
-const wrapped = test().wrap(calculateAttack);
+const wrapped = fftest().wrap(calculateAttack);
 
 beforeAll(async () => {
     admin.initializeApp();
@@ -29,13 +29,17 @@ beforeAll(async () => {
 beforeEach(() => {
     const testAttacker = getCharacterRepr();
     const testDefender = getCharacterRepr();
-    testDefender['weapons'] = [];  // overwrite so one has no weapons
+
+    // overwrite so one has no attacks
+    testDefender['weapons'] = [];
+    testDefender['abilities'] = [];
 
     testWeapon = testAttacker['weapons'][0];
+    testAbility = testAttacker['abilities'][0];
     testData = {
         attackerId: 'attacker',
         defenderId: 'defender',
-        weaponName: testWeapon['name'],
+        attackName: testWeapon['name'],
         roll: 5,
     };
 
@@ -49,44 +53,72 @@ afterAll(async () => {
     await testEnv.clearFirestore();
 });
 
-it('attacker does not exist', async () => {
+test('attacker does not exist', async () => {
     await testCollection.doc('attacker').delete();
     await expect(wrapped(testData)).rejects.toThrow('Could not find attacker');
 });
 
-it('defender does not exist', async () => {
+test('defender does not exist', async () => {
     await testCollection.doc('defender').delete();
     await expect(wrapped(testData)).rejects.toThrow('Could not find defender');
 });
 
-it('weaponId does not exist on attacker', async () => {
-    testData.attackerId = 'defender';
-    await expect(wrapped(testData)).rejects.toThrow('Could not find weapon');
+test('attack name does not exist on attacker', async () => {
+    const attackerNoWeapons = getCharacterRepr();
+    attackerNoWeapons.weapons = [];
+    await testCollection.doc('attacker').set(attackerNoWeapons);
+
+    await expect(wrapped(testData)).rejects.toThrow('Could not find weapon or ability');
 });
 
-it('calls downstream and sends correct response', async () => {
+describe('calls downstream for weapon and sends correct response', () => {
     const mockCalculateToHit = jest.spyOn(attackCalculator, 'calculateToHit')
         .mockImplementation(() => { return {doesAttackHit: true, attackerToHit: 10, defenderEvade: 2}; });
     const mockCalculateDamage = jest.spyOn(attackCalculator, 'calculateDamage')
         .mockImplementation(() => 3);
 
-    const result = await wrapped(testData);
+    test('weapon', async () => {
+        const result = await wrapped(testData);
 
-    expect(mockCalculateToHit).toHaveBeenCalledWith(
-        5,
-        expect.any(Character),
-        expect.any(Character),
-        testWeapon,
-    );
-    expect(mockCalculateDamage).toHaveBeenCalledWith(
-        expect.any(Character),
-        expect.any(Character),
-        testWeapon,
-    );
-    expect(result).toEqual({
-        doesAttackHit: true,
-        attackerToHit: 10,
-        defenderEvade: 2,
-        damage: 3,
+        expect(mockCalculateToHit).toHaveBeenCalledWith(
+            5,
+            expect.any(Character),
+            expect.any(Character),
+            testWeapon,
+        );
+        expect(mockCalculateDamage).toHaveBeenCalledWith(
+            expect.any(Character),
+            expect.any(Character),
+            testWeapon,
+        );
+        expect(result).toEqual({
+            doesAttackHit: true,
+            attackerToHit: 10,
+            defenderEvade: 2,
+            damage: 3,
+        });
+    });
+
+    test('ability', async () => {
+        testData['attackName'] = testAbility.name;
+        const result = await wrapped(testData);
+
+        expect(mockCalculateToHit).toHaveBeenCalledWith(
+            5,
+            expect.any(Character),
+            expect.any(Character),
+            testAbility,
+        );
+        expect(mockCalculateDamage).toHaveBeenCalledWith(
+            expect.any(Character),
+            expect.any(Character),
+            testAbility,
+        );
+        expect(result).toEqual({
+            doesAttackHit: true,
+            attackerToHit: 10,
+            defenderEvade: 2,
+            damage: 3,
+        });
     });
 });
--- functions/src/src_firebase_functions/calculate_attack.ts
+++ functions/src/src_firebase_functions/calculate_attack.ts
@@ -3,12 +3,33 @@ import * as functions from 'firebase-functions';
 
 import { calculateDamage, calculateToHit } from 'attack_calculator';
 import { characterDataConverter } from 'firestore_utils/data_converters';
+import { getNonNull } from 'utils';
+
+function getAttack<T extends {name: string}>(name: string, attackType: string, attackList: T[]) : T | null {
+    let attack = null;
+    for (const a of attackList) {
+        if (a.name !== name) {
+            continue;
+        }
+
+        if (attack !== null) {
+            throw new functions.https.HttpsError(
+                'failed-precondition',
+                `Character is invalid, found multiple ${attackType} objects with name ${name}`
+            );
+        }
+
+        attack = a;
+    }
+
+    return attack;
+}
 
 /*
 request.query: {
     attackerId: string,
     defenderId: string,
-    weaponName: string,
+    attackName: string,
     roll: number,
 }
 response: {
@@ -18,11 +39,10 @@ response: {
     damage: number,
 }
  */
-
 export default functions.https.onCall(async (data) => {
     const attackerId = data.attackerId as string;
     const defenderId = data.defenderId as string;
-    const weaponName = data.weaponName as string;
+    const attackName = data.attackName as string;
     const roll = parseInt(data.roll as string);
 
     const charCollection = admin.firestore().collection('Characters');
@@ -40,23 +60,25 @@ export default functions.https.onCall(async (data) => {
     const attacker = characterDataConverter.fromFirestore(attackerDoc as admin.firestore.QueryDocumentSnapshot);
     const defender = characterDataConverter.fromFirestore(defenderDoc as admin.firestore.QueryDocumentSnapshot);
 
-    let weapon = null;
-    for (const w of attacker.getWeapons()) {
-        if (w.name === weaponName) {
-            weapon = w;
-            break;
-        }
-    }
+    const weapon = getAttack(attackName, 'weapon', attacker.data.weapons);
+    const ability = getAttack(attackName, 'ability', attacker.data.abilities);
 
-    if (weapon === null) {
+    if (weapon === null && ability === null) {
         throw new functions.https.HttpsError(
-            'invalid-argument', 
-            `Could not find weapon on attacker with name ${weaponName}`
+            'invalid-argument',
+            `Could not find weapon or ability with name ${attackName} on attacker`
+        );
+    }
+    if (weapon !== null && ability !== null) {
+        throw new functions.https.HttpsError(
+            'failed-precondition',
+            `Character is invalid, found both a weapon and an ability with name ${attackName}`
         );
     }
 
-    const toHitResult = calculateToHit(roll, attacker, defender, weapon);
-    const damage = calculateDamage(attacker, defender, weapon);
+    const attack = getNonNull((weapon !== null) ? weapon : ability);
+    const toHitResult = calculateToHit(roll, attacker, defender, attack);
+    const damage = calculateDamage(attacker, defender, attack);
 
     return {
         doesAttackHit: toHitResult.doesAttackHit,
